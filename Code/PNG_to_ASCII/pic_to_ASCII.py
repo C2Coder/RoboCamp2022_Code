d7d8eddcad16c47a417a34359104ab311cccd614
C2Coder <102322220+C2Coder@users.noreply.github.com>
2023-03-05 15:19:56 +0100

    Delete Code directory

--- Code/PNG_to_ASCII/pic_to_ASCII.py
+++
@@ -1,8 +0,0 @@
-import ascii_magic  # pip install ascii_magic
-
-input_path = 'C:/Users/vojte/Desktop/PNG_to_ASCII/input.png'
-output_path = 'C:/Users/vojte/Desktop/PNG_to_ASCII/output.txt'
-
-ASCII_image = ascii_magic.from_image_file(input_path)
-ascii_magic.to_terminal(ASCII_image)
-ascii_magic.to_file(output_path, ASCII_image)
